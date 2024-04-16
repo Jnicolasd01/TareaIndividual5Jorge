93c6b36b254613f2c2f77a84a285e1aad2974ede
unknown <DAM116@iesataulfo.local>
2024-04-16 15:29:06 +0200

    Primera fase proyecto

---
+++ src/directorioTrabajoNicolasJorge/Clase.java
@@ -0,0 +1,5 @@
+package directorioTrabajoNicolasJorge;
+
+public class Clase {
+
+}
---
+++ src/directorioTrabajoNicolasJorge/Principal.java
@@ -0,0 +1,10 @@
+package directorioTrabajoNicolasJorge;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
